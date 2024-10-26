f493dfac0d73b1dca337d4186c243d3e894f07f3
EliasLGT <52158982+EliasLGT@users.noreply.github.com>
2024-10-26 16:12:13 +0300

    Runner executes routine and is able to greet(No daynight cycle)

--- VirtualRealityApallaktikiP20114/Assets/Mixamo/Animations/runner/RoutineOfRunner.cs
+++ VirtualRealityApallaktikiP20114/Assets/Mixamo/Animations/runner/RoutineOfRunner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,7 +11,8 @@ public class RoutineOfRunner : MonoBehaviour
     public GameObject PATH;
     private Transform[] PathPoints;
     public int index = 0;
-    public float minDistance = 10;
+    public float minDistance = 0;
+    public int[] indeces = {2, 3, 4};
     // Start is called before the first frame update
     void Start()
     {
@@ -28,15 +30,64 @@ public class RoutineOfRunner : MonoBehaviour
     }
 
     void roam(){
-        if(Vector3.Distance(transform.position, PathPoints[index].position) < minDistance){
-            if(index > 0 && index < PathPoints.Length){
+        if(Vector3.Distance(transform.position, PathPoints[index].position) <= minDistance){
+            if(index >= 0 && index < PathPoints.Length - 1){
                 index += 1;
+                if(indeces.Contains(index)){
+                    Jog();
+                }
+                else{
+                    Walk();
+                }
             }
             else{
-                index = 0;
+                index = 2;
             }
         }
 
         agent.SetDestination(PathPoints[index].position);
+
+        
     }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Player")){
+            agent.isStopped = true;
+            Wave();
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other.CompareTag("Player")){
+            Stop();
+            if(indeces.Contains(index)){
+                Jog();
+            }
+            else{
+                Walk();
+            }
+            agent.isStopped = false;
+            agent.SetDestination(PathPoints[index].position);
+        }
+    }
+
+    private void Walk(){
+        animator.SetTrigger("Walk");
+    }
+
+    private void Jog(){
+        animator.SetTrigger("Jog");
+    }
+
+    private void Stop(){
+        animator.SetTrigger("Stop");
+    }
+
+    private void Wave(){
+        animator.SetTrigger("Wave");
+    }
+
+    // private void stopWaving(){
+    //     animator.SetTrigger("stopWaving");
+    // }
 }
